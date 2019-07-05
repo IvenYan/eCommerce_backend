474057a2b9b114b79713dd092a63b5f31c6233d4
yankun <yankun_smile@sina.com>
2019-07-06 00:14:42 +0800

    111

--- src/main/java/com/management/backend/api/util/UtilTools.java
+++ src/main/java/com/management/backend/api/util/UtilTools.java
@@ -2,6 +2,7 @@ package com.management.backend.api.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 import java.util.SimpleTimeZone;
 import java.util.TimeZone;
 
@@ -34,7 +35,31 @@ public class UtilTools {
     }
 
     public static void main(String[] args) {
-        String formatedDateString = UtilTools.getFormatedDateString(0.00f);
-        System.out.println(formatedDateString);
+//        String formatedDateString = UtilTools.getFormatedDateString(0.00f);
+//        System.out.println(formatedDateString);
+        for (int i = 0; i < 90; i++) {
+            String s = UtilTools.generatePassword(16);
+            System.out.println(s);
+        }
+
+    }
+
+    public static String generatePassword (int length) {
+        // 最终生成的密码
+        String password = "";
+        Random random = new Random();
+        for (int i = 0; i < length; i ++) {
+            // 随机生成0或1，用来确定是当前使用数字还是字母 (0则输出数字，1则输出字母)
+            int charOrNum = random.nextInt(2);
+            if (charOrNum == 1) {
+                // 随机生成0或1，用来判断是大写字母还是小写字母 (0则输出小写字母，1则输出大写字母)
+                int temp = random.nextInt(2) == 1 ? 65 : 97;
+                password += (char) (random.nextInt(26) + temp);
+            } else {
+                // 生成随机数字
+                password += random.nextInt(10);
+            }
+        }
+        return password;
     }
 }
